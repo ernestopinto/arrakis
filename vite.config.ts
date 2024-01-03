bc9514711b50f1eb214062c4ea0ab9a119857a17
Ernesto Pinto <ernesto.pinto@devoteam.com>
2024-01-03 15:00:02 +0000

    docs deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: 'docs',
+  },
+  base: process.env.NODE_ENV === 'production' ? '/arrakis/' : '/',
 })
